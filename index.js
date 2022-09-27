fc347ad96c053fcdd13d3b569b88562b8074598c
robertp77 <robert.a.paul99@gmail.com>
2022-09-26 22:40:48 -0400

    test

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/', (req, res) => res.render('src/main.js'))
+  .set('view engine', 'html')
+  .get('/', (req, res) => res.render('dist/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
