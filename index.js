e670c129bf4dc50922e30c9cc4649d1ba043f1db
robertp77 <robert.a.paul99@gmail.com>
2022-09-26 23:15:35 -0400

    test

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const PORT = process.env.PORT || 5000
-
-express()
-  .set('view engine', 'html')
-  .get('/', (req, res) => res.render('dist/index.html'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
